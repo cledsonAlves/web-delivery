4fc0c5c057142681e4d8b6551e38fdfa76a05dce
Cledson Alves dos Santos <cledson@Mac-mini-de-Cledson.local>
2026-01-04 12:53:30 -0300

    auto

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-    base: process.env.NODE_ENV === 'production'
-    ? '/web-delivery/'
-    : '/',
+    base:  './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
